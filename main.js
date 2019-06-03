b25b76d12532f4567dbb250b247763023f333cfb
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2019-06-03 15:20:30 +0200

    MF first refactoring step

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function statement (invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = calculateBill(play.type, play.audience);
+        let thisAmount = amountFor(perf, play);
        // add volume credits
  volumeCredits += Math.max(perf.audience - 30, 0);
  // add extra credit for every ten comedy attendees
@@ -22,24 +22,24 @@ function statement (invoice, plays) {
  return result;
  }
 
- function calculateBill(aPerformance) {
+ function amountFor(perf, play) {
  let thisAmount = 0;
-        switch (aPerformance, audience) {
-            case "tragedy":
-            thisAmount = 40000;
-            if (audience > 30) {
-            thisAmount += 1000 * (audience - 30);
-            }
-            break;
-            case "comedy":
-            thisAmount = 30000;
-            if (audience > 20) {
-            thisAmount += 10000 + 500 * (audience - 20);
-            }
-            thisAmount += 300 * audience;
-            break;
-            default:
-            throw new Error(`unknown type: ${aPerformance}`);
-            }
+        switch (play.type) {
+        case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+        case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+        default:
+        throw new Error(`unknown type: ${play.type}`);
+        }
     return thisAmount;
 }
