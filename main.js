44fe03f4d130e247470d366f92f220acf70398ec
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-06-17 17:25:59 +0200

    Added volumeCreditsFor function and extracted logic of volumeCredit calculations from statement function

--- main.js
+++ main.js
@@ -15,10 +15,8 @@ function statement (invoice, plays) {
     minimumFractionDigits: 2 }).format;
 
     for (let perf of invoice.performances) {
-       // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+       
+        volumeCredits += volumeCreditsFor(perf);
         // print line for this order
         result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
@@ -26,6 +24,15 @@ function statement (invoice, plays) {
  result += `Amount owed is ${format(totalAmount/100)}\n`;
  result += `You earned ${volumeCredits} credits\n`;
  return result;
+ }
+
+ function volumeCreditsFor(perf) {
+     let volumeCredits = 0;
+     volumeCredits += Math.max(perf.audience - 30, 0);
+     if ("comedy" === playFor(perf).type) 
+        volumeCredits += Math.floor(perf.audience / 5);
+
+     return volumeCredits;   
  }
 
  function amountFor(aPerformance) {
