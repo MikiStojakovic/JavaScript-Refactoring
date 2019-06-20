1b24e65b3b8d87f7ae6a11b34c72c2a545cbba29
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-06-20 15:51:27 +0200

    Extracted volume credits calculation to separate function

--- main.js
+++ main.js
@@ -7,23 +7,27 @@ function testStatement(){
 }
 
 function statement (invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
+    let totalAmount = 0;    
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-    { style: "currency", currency: "USD",
-    minimumFractionDigits: 2 }).format;
 
     for (let perf of invoice.performances) {
-       
-        volumeCredits += volumeCreditsFor(perf);
         // print line for this order
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
- result += `Amount owed is ${usd(totalAmount)}\n`;
- result += `You earned ${volumeCredits} credits\n`;
- return result;
+
+    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
+    return result;
+ }
+
+ function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {       
+        volumeCredits += volumeCreditsFor(perf);
+    }
+
+    return volumeCredits;
  }
 
  function volumeCreditsFor(aPerformance) {
