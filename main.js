f0f0370b10058320ffbeb6c53a508aa185e45daf
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-06-25 16:13:25 +0200

    Replaced volumeCreditsFor function call with inermediate object data

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function statement (invoice, plays) {
         function totalVolumeCredits() {
             let result = 0;
             for (let perf of data.performances) {       
-                result += volumeCreditsFor(perf);
+                result += perf.volumeCredits;
             }
         
             return result;
@@ -87,6 +87,7 @@ function statement (invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
