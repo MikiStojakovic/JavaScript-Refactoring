54c72a97a21c8551323babdee89676500d15a934
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-06-26 11:09:11 +0200

    Moved createStatementData function to its separate file

--- main.js
+++ main.js
@@ -1,20 +1,13 @@
 
 var invoiceCollection = require('./invoices.json');
 var playsCollection = require('./plays.json');
+var createStatementData = require('./createStatementData.js').createStatementData;
 
 function testStatement(){
     return statement(invoiceCollection, playsCollection);
 }
 
-function createStatementData(invoice, plays){
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances =
-        invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;    
-}
+
 
 function statement (invoice, plays) {
    
@@ -39,57 +32,6 @@ function statement (invoice, plays) {
            minimumFractionDigits: 2 }).format(aNumber/100);
    }  
 } 
-
- function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-}
-
-function playFor(aPerformance) {
-    return playsCollection[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-    let result = 0;
-           switch (aPerformance.play.type) {
-           case "tragedy":
-           result = 40000;
-           if (aPerformance.audience > 30) {
-               result += 1000 * (aPerformance.audience - 30);
-           }
-           break;
-           case "comedy":
-           result = 30000;
-           if (aPerformance.audience > 20) {
-               result += 10000 + 500 * (aPerformance.audience - 20);
-           }
-           result += 300 * aPerformance.audience;
-           break;
-           default:
-           throw new Error(`unknown type: ${aPerformance.play.type}`);
-           }
-       return result;
-   }
-
-   function totalAmount(data){
-    return data.performances.reduce((total, p) => total + p.amount, 0)
-}
-
-function totalVolumeCredits(data){
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
- }
-
- function volumeCreditsFor(aPerformance) {
-     let result = 0;
-     result += Math.max(aPerformance.audience - 30, 0);
-     if ("comedy" === aPerformance.play.type) 
-        result += Math.floor(aPerformance.audience / 5);
-
-     return result;   
- } 
    
 // function add(a, b) {
 //     return a + b;
