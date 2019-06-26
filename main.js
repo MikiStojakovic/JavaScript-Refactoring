9fe69c1cd111132ffc7791b1c4b3260872ed62c0
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-06-26 10:43:13 +0200

    Extract render plain text as separate function

--- main.js
+++ main.js
@@ -18,37 +18,27 @@ function createStatementData(invoice, plays){
 
 function statement (invoice, plays) {
    
-    return renderPlainText(createStatementData(invoice, plays));    
-
-    // function createStatementData(invoice, plays){
-    //     const statementData = {};
-    //     statementData.customer = invoice.customer;
-    //     statementData.performances =
-    //         invoice.performances.map(enrichPerformance);
-    //     statementData.totalAmount = totalAmount(statementData);
-    //     statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    //     return statementData;    
-    // }
-
-    function renderPlainText(data, plays) {
-        let result = `Statement for ${data.customer}\n`;    
-    
-        for (let perf of data.performances) {
-            // print line for this order
-            result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
-        }
-    
-        result += `Amount owed is ${usd(data.totalAmount)}\n`;
-        result += `You earned ${data.totalVolumeCredits} credits\n`;
-        return result;
-                                                                   
-       function usd(aNumber){
-           return new Intl.NumberFormat("en-US",
-               { style: "currency", currency: "USD",
-               minimumFractionDigits: 2 }).format(aNumber/100);
-       }  
-    }    
+    return renderPlainText(createStatementData(invoice, plays));       
  }
+
+ function renderPlainText(data, plays) {
+    let result = `Statement for ${data.customer}\n`;    
+
+    for (let perf of data.performances) {
+        // print line for this order
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
+    result += `You earned ${data.totalVolumeCredits} credits\n`;
+    return result;
+                                                               
+   function usd(aNumber){
+       return new Intl.NumberFormat("en-US",
+           { style: "currency", currency: "USD",
+           minimumFractionDigits: 2 }).format(aNumber/100);
+   }  
+} 
 
  function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
