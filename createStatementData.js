e62e664dccd57562b8b516bfb2ab0fd01f7bc844
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-06-27 14:42:15 +0200

    Added volumeCredits override functino to TragedyCalculator; changed volumeCredits in PerformanceCaluculator

--- createStatementData.js
+++ createStatementData.js
@@ -27,14 +27,9 @@ class PerformanceCalculator {
        }
 
        get volumeCredits() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" === this.play.type) 
-           result += Math.floor(this.performance.audience / 5);
-   
-        return result;   
-    } 
-}
+           return Math.max(this.performance.audience - 30, 0);
+       }
+    }
 
 class TragedyCalculator extends PerformanceCalculator {    
     constructor(aPerformance, aPlay){
@@ -70,6 +65,10 @@ class ComedyCalculator extends PerformanceCalculator {
 
         result += 300 * this.performance.audience;
         return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
     }
 }
 
