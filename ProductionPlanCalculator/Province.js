1fa93f358109cc0238108195e2d01ce089fe521d
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-07-18 16:39:35 +0200

    Added sample data method; Added geters and setters to Province class

--- ProductionPlanCalculator/Province.js
+++ ProductionPlanCalculator/Province.js
@@ -8,8 +8,30 @@ class Province {
         doc.producers.forEach(d => this.addProducer(new Producer(this, d)));
     }
 
+    get name() { return this._name;}
+    get producers() {return this._producers.slice();}
+    get totalProduction() {return this._totalProduction;}
+    set totalProduction(arg) {this._totalProduction = arg;}
+    get demand() {return this._demand;}
+    set demand(arg) {return this._demand = parseInt(arg);}
+    get price() {return this._price;}
+    set price(arg) {this._price = parseInt(arg);}
+
     addProducer(arg) {
         this._producers.push(arg);
         this._totalProduction += arg.production;
-    }
+    }    
+}
+
+function sampleProvinceData() {
+    return {
+        name: 'Asia',
+        producers: [
+            {name: "Byzantium", cost: 10, production: 9},
+            {name: "Attalia", cost: 12, production: 10},
+            {name: "Sinope", cost: 10, production: 6}
+        ],
+        demand: 30,
+        price: 20
+    };
 }
