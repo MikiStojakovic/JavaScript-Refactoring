9c54fda630b02f000c5249fd05300a44b41d7188
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-07-18 17:15:00 +0200

    Added new geters and setters methods

--- ProductionPlanCalculator/Province.js
+++ ProductionPlanCalculator/Province.js
@@ -16,6 +16,26 @@ class Province {
     set demand(arg) {return this._demand = parseInt(arg);}
     get price() {return this._price;}
     set price(arg) {this._price = parseInt(arg);}
+    get shortfall() { return this._demand - this.totalProduction; }
+    get profit() {return this.demandValue - this.demandCost;}
+    get demandCost() {
+        let remainingDemand = this.demand;
+        let result = 0;
+        this.producers
+            .sort((a, b) => a.cost - b.cost)
+            .forEach(p => {
+                const contribution = Math.min(remainingDemand, p.production);
+                remainingDemand -= contribution;
+                result += contribution * p.cost;
+            });
+        return result;            
+    }
+    get demandValue() {
+        return this.satisfiedDemand * this.price;
+    }
+    get satisfiedDemand() {
+        return Math.min(this._demand, this.totalProduction);
+    }
 
     addProducer(arg) {
         this._producers.push(arg);
