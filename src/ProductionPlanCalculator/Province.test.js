3c349a9784ab94c3396d70da513b44b0de7cc574
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-07-25 21:29:33 +0200

    Added unit test for shortfall and profit methods of Province class with empty producers array

--- src/ProductionPlanCalculator/Province.test.js
+++ src/ProductionPlanCalculator/Province.test.js
@@ -12,3 +12,25 @@
         expect(asia.profit).toBe(230);
     });
  })
+
+ describe('no producers', function(){
+     let noProducers;
+
+     beforeEach(function(){
+         const data = {
+             name: "No producers",
+             producers: [],
+             demand: 30,
+             price: 20
+         };
+         noProducers = new Province(data);
+     });
+
+     it('shortfall', function() {
+        expect(noProducers.shortfall).toBe(30);        
+     });
+
+     it('profit', function() {
+         expect(noProducers.profit).toBe(0);
+     })
+ })
