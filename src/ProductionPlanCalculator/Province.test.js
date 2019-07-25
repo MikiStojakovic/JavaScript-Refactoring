f4272d71be1e2d61c5220d5e6c6e6ae73054f0f2
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-07-25 16:30:37 +0200

    Added unit test for profit method of province class

--- src/ProductionPlanCalculator/Province.test.js
+++ src/ProductionPlanCalculator/Province.test.js
@@ -4,5 +4,9 @@
      it('shortfall', function(){
          const asia = new Province(sampleProvinceData());
          expect(asia.shortfall).toBe(5);
-     })
+     });
+     it('profit', function(){
+        const asia = new Province(sampleProvinceData());
+        expect(asia.profit).toBe(230);
+    });
  })
