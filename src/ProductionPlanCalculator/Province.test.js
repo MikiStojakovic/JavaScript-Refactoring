aca7d39c92862b19f05d75f07c6a8ce1bee570db
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-07-25 16:55:14 +0200

    Added beforeEach clause to test file to instantiate new object before every unit test

--- src/ProductionPlanCalculator/Province.test.js
+++ src/ProductionPlanCalculator/Province.test.js
@@ -1,12 +1,14 @@
  import { sampleProvinceData, Province } from './Province.js';
 
  describe('province shortfall', function() {
+     let asia;
+     beforeEach(function() {
+         asia = new Province(sampleProvinceData());
+     })
      it('shortfall', function(){
-         const asia = new Province(sampleProvinceData());
          expect(asia.shortfall).toBe(5);
      });
      it('profit', function(){
-        const asia = new Province(sampleProvinceData());
         expect(asia.profit).toBe(230);
     });
  })
